5c415baf46750c578c58e7ad78df119d04dac393
AhmedMohsen600 <ahmedmohsen159357@gmail.com>
2023-01-28 12:35:33 +0200

    Create navbar and sidebar

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { logo } from "../util/constant";
+import SearchBar from "./SearchBar";
+
+function Navbar() {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        top: 0,
+        background: "#000",
+        justifyContent: "space-between",
+      }}
+    >
+      <Link style={{ display: "flex", alignItems: "center" }} to="/">
+        <img height={45} src={logo} alt="Logo" />
+      </Link>
+      <SearchBar></SearchBar>
+    </Stack>
+  );
+}
+
+export default Navbar;
